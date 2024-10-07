94b24b3060eec41446a83b82ad13f15aabc6fb45
Ffion <tesnileft@gmail.com>
2024-10-07 07:12:48 +0200

    Block class

--- P_2 Tetris/Block.cs
+++ P_2 Tetris/Block.cs
@@ -1,6 +1,35 @@
+using System.Collections.Generic;
+using System.Numerics;
+using Microsoft.Xna.Framework;
+
+
 namespace P_2_Tetris;
 
 public class Block
 {
+    Point Position;
+    
+    static Dictionary<BlockShape, Matrix> blocks = new Dictionary<BlockShape, Matrix>();
+    enum BlockShape
+    {
+        Square,
+        Long
+    }
+
+    struct BlockDefinition
+    {
+        private Color Color;
+        
+
+    }
+    Block(BlockShape blockShape)
+    {
+        
+    }
+
+    void Rotate()
+    {
+        
+    }
     
 }
--- P_2 Tetris/BlockGrid.cs
+++ P_2 Tetris/BlockGrid.cs
@@ -2,5 +2,8 @@ namespace P_2_Tetris;
 
 public class BlockGrid
 {
-    
+    private void InsertBlock(Block block)
+    {
+        
+    }
 }
